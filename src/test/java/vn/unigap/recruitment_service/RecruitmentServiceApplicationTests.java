f72829fda92476442b6ceb286774a0c6ee2ab82f
wiehoang <ndhhoang1210@gmail.com>
2024-06-26 22:40:02 +0700

    Initialize commit

---
+++ src/test/java/vn/unigap/recruitment_service/RecruitmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.unigap.recruitment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
