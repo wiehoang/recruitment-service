796faa3e43002680cf4d8ce59541be548c0246fc
Wie <wie@Wies-MacBook-Air.local>
2024-08-14 21:41:59 +0700

    Enable caching with Redis

--- src/main/java/vn/unigap/RecruitmentServiceApplication.java
+++ src/main/java/vn/unigap/RecruitmentServiceApplication.java
@@ -3,11 +3,13 @@ package vn.unigap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 //import vn.unigap.api.service.migration.DatabaseMigrationService;
 
 
 @SpringBootApplication
+@EnableCaching
 public class RecruitmentServiceApplication {
 
 	public static void main(String[] args) {
